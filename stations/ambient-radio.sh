dde75d76eb007ae58d2bb1b3edbe9f8df60e3561
Kevin Luzbetak <luzbetak5739@gmail.com>
2023-06-15 14:32:30 -0700

    2023-06-15 14:32

--- stations/ambient-radio.sh
+++ stations/ambient-radio.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #
-# https://www.internet-radio.com/stations/techno/page2
+# https://www.internet-radio.com/stations/ambient/page3
 mpc clear
+mpc add http://174.36.206.197:7278/stream/1/ 
 mpc add http://62.149.196.16:8888/stream/1/ 
 mpc add http://s10.voscast.com:9574/stream/1/ 
 mpc add http://cast4.my-control-panel.com:7062/autodj 
--- stations/slovak-radio.sh
+++ stations/slovak-radio.sh
@@ -2,7 +2,13 @@
 #
 # https://www.internet-radio.com/stations/techno/page2
 mpc clear
+
 mpc add http://stream.funradio.sk:8000/fun128.mp3
+mpc add http://live.slovakradio.sk:8000/Slovensko_256.mp3
+mpc add http://live.slovakradio.sk:8000/Devin_256.mp3
+
+
+
 mpc save slovakia 
 mpc play
 
