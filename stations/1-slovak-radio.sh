30aa14f858c2157dfdb47d237f6dda9106d9ad3d
Kevin Luzbetak <luzbetak5739@gmail.com>
2023-06-16 10:45:16 -0700

    2023-06-16 10:45

--- stations/1-slovak-radio.sh
+++ stations/1-slovak-radio.sh
@@ -3,14 +3,16 @@
 # https://www.internet-radio.com/stations/techno/page2
 mpc clear
 
-mpc add http://live.slovakradio.sk:8000/Devin_256.mp3
+mpc add http://stream.funradio.sk:8000/fun128.mp3
 mpc add http://live.slovakradio.sk:8000/FM_256.mp3
 mpc add http://live.slovakradio.sk:8000/Slovensko_256.mp3
-mpc add http://stream.funradio.sk:8000/fun128.mp3
+mpc add http://live.slovakradio.sk:8000/Devin_256.mp3
+mpc add https://stream.expres.sk/128.mp3                    # Rádio Expres Bratislava, Slovakia
+mpc add https://stream.radiovlna.sk/vlna-hi.mp3             # Rádio Vlna Slovakia Bratislava
 
 mpc rm slovakia 
 mpc save slovakia 
-mpc random on
+mpc random off 
 mpc play
 
 
